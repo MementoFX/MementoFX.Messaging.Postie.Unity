76594e344965221b8e2d85715db30016fe63b71d
Andrea Saltarello <andysal@gmail.com>
2017-03-10 18:27:44 +0100

    1.0.1

---
+++ src/MementoFX.Messaging.Postie.Unity.Tests/UnityTypeResolverFixture.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Practices.Unity;
+using Moq;
+using NUnit.Framework;
+using SharpTestsEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento.Messaging.Postie.Unity.Tests
+{
+    [TestFixture]
+    public class UnityTypeResolverFixture
+    {
+        [Test]
+        public void Ctor_should_throw_ArgumentNullException_on_null_container_parameter()
+        {
+            Executing.This(() => new UnityTypeResolver(null))
+                .Should()
+                .Throw<ArgumentNullException>()
+                .And
+                .ValueOf
+                .ParamName
+                .Should()
+                .Be
+                .EqualTo("container");
+        }
+
+        [Test]
+        public void Ctor_should_set_Container_property()
+        {
+            var containerMock = new Mock<IUnityContainer>().Object;
+            var sut = new UnityTypeResolver(containerMock);
+            Assert.AreSame(containerMock, sut.Container);
+        }
+    }
+}
---
+++ src/MementoFX.Messaging.Postie.Unity/UnityTypeResolver.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento.Messaging.Postie.Unity
+{
+    /// <summary>
+    /// Defines a Unity-based type resolver for Postie
+    /// </summary>
+    public class UnityTypeResolver : ITypeResolver
+    {
+        /// <summary>
+        /// The container instance used ty resolver types
+        /// </summary>
+        public IUnityContainer Container { get; private set; }
+
+        /// <summary>
+        /// Creates a new instance of the type resolver
+        /// </summary>
+        /// <param name="container">The container instance used to resolve types</param>
+        public UnityTypeResolver(IUnityContainer container)
+        {
+            if (container == null)
+                throw new ArgumentNullException(nameof(container));
+            Container = container;
+        }
+
+        /// <summary>
+        /// Resolves an instance of the requested type with the given name from the container.
+        /// </summary>
+        /// <param name="t">Type of object to get from the container.</param>
+        /// <returns>The retrieved object.</returns>
+        public object Resolve(Type t)
+        {
+            return Container.Resolve(t);
+        }
+    }
+}
