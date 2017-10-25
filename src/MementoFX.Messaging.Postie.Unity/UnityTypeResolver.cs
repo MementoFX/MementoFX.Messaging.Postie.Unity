727282a47d24c67e154abc806b5b833ac84d92f9
Andrea Saltarello <andysal@gmail.com>
2017-10-25 10:31:03 +0200

    v2.0.0-pre1

--- src/MementoFX.Messaging.Postie.Unity/UnityTypeResolver.cs
+++ src/MementoFX.Messaging.Postie.Unity/UnityTypeResolver.cs
@@ -1,10 +1,5 @@
-﻿using Memento.Messaging.Postie;
-using Microsoft.Practices.Unity;
+﻿using Microsoft.Practices.Unity;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MementoFX.Messaging.Postie.Unity
 {
