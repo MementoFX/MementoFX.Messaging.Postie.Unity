dea764ecaaae2cc3e5caa44713ea6c6a7a41f4cd
Andrea Saltarello <andysal@users.noreply.github.com>
2018-11-14 19:08:39 +0100

    v2.0.0-pre2

--- src/MementoFX.Messaging.Postie.Unity.Tests/Properties/AssemblyInfo.cs
+++ src/MementoFX.Messaging.Postie.Unity.Tests/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("MementoFX.Messaging.Postie.Unity.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MementoFX.Messaging.Postie.Unity.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Managed Designs")]
+[assembly: AssemblyProduct("MementoFX")]
+[assembly: AssemblyCopyright("© 2013-2018 Andrea Saltarello, Managed Designs")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
